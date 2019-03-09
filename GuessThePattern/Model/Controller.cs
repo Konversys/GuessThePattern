453eeaab0f58de20ad9956dd63da886221ee89dc
Халилов Рустам - LN <konversys@gmail.com>
2019-03-10 04:32:46 +0700

    Добавлен контроллер

---
+++ GuessThePattern/Model/Controller.cs
@@ -0,0 +1,70 @@
+﻿using GuessThePattern.Model.Models;
+using System;
+
+namespace GuessThePattern.Model
+{
+    public class Controller
+    {
+        public int RightAnswer { get; private set; }
+        int rightAnswers { get; set; }
+        public void TakeRightAnwer()
+        {
+            rightAnswers++;
+        }
+        bool IsStart { get; set; }
+        public int Count { get; private set; }
+        public readonly int Max;
+        public Pattern[] Patterns;
+        Patterns patterns;
+        Random random;
+        public Controller()
+        {
+            random = new Random();
+            patterns = new Patterns();
+            Patterns = new Pattern[4];
+            Max = 10;
+            rightAnswers = 0;
+            IsStart = false;
+        }
+
+        public void Start()
+        {
+            IsStart = true;
+        }
+
+        public Pattern GetRightPattern()
+        {
+            return Patterns[RightAnswer];
+        }
+
+        public bool CheckIsStart()
+        {
+            if ((Count == 10 && IsStart == true) || IsStart == false)
+            {
+                Count = 0;
+                IsStart = false;
+                return false;
+            }
+            return true;
+        }
+
+        public void Next()
+        {
+            Patterns = patterns.GetRandom();
+            RightAnswer = random.Next(Patterns.Length);
+            Count++;
+        }
+
+        public int GetScore()
+        {
+            return 100 * rightAnswers / Max;
+        }
+
+        public bool DetermineTask()
+        {
+            if (random.Next(2) == 0)
+                return true;
+            return false;
+        }
+    }
+}
--- Test/ControllerTest.cs
+++ Test/ControllerTest.cs
@@ -13,17 +13,20 @@ namespace Test
             Controller controller = new Controller();
             Assert.IsNotNull(controller);
         }
+        [TestMethod]
         public void ControllerNotStart()
         {
             Controller controller = new Controller();
             Assert.IsFalse(controller.CheckIsStart());
         }
+        [TestMethod]
         public void ControllerStart()
         {
             Controller controller = new Controller();
             controller.Start();
             Assert.IsTrue(controller.CheckIsStart());
         }
+        [TestMethod]
         public void ControllerNextPattern()
         {
             Controller controller = new Controller();
@@ -31,6 +34,7 @@ namespace Test
             controller.Next();
             Assert.IsNotNull(controller.Patterns);
         }
+        [TestMethod]
         public void ControllerNextRAnswer()
         {
             Controller controller = new Controller();
@@ -38,6 +42,7 @@ namespace Test
             controller.Next();
             Assert.IsNotNull(controller.RightAnswer);
         }
+        [TestMethod]
         public void ControllerComplete()
         {
             Controller controller = new Controller();
@@ -46,31 +51,36 @@ namespace Test
             {
                 controller.Next();
             }
-            Assert.IsFalse(controller.CheckIsStart());
+            Assert.IsTrue(controller.CheckIsStart());
         }
+        [TestMethod]
         public void ControllerScope()
         {
             Controller controller = new Controller();
             controller.Start();
             controller.TakeRightAnwer();
             controller.TakeRightAnwer();
-            Assert.AreEqual(controller.GetScore(), (int)100 * 2 / controller.Max);
+            Assert.AreEqual((int)100 * 2 / controller.Max, controller.GetScore());
         }
+        [TestMethod]
         public void ControllerCount()
         {
             Controller controller = new Controller();
             controller.Start();
             controller.Next();
             controller.Next();
-            Assert.AreEqual(controller.Count, 3);
+            Assert.AreEqual(2, controller.Count);
         }
+        [TestMethod]
         public void ControllerRightPattern()
         {
             Controller controller = new Controller();
             controller.Start();
-            Assert.IsNotNull(controller.GetRightPattern());
+            controller.Next();
+            Assert.IsInstanceOfType(controller.RightAnswer, typeof(int));
         }
-        public void ControllerRightPattern()
+        [TestMethod]
+        public void ControllerDetermineTask()
         {
             Controller controller = new Controller();
             controller.Start();
