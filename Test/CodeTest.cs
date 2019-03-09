b88d8faa1764ba665caf6675d1e38695b65c0d06
Халилов Рустам - LN <konversys@gmail.com>
2019-03-10 02:14:27 +0700

    Тесты кода

---
+++ Test/CodeTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class CodeTest
+    {
+        [TestMethod]
+        public void TestAdaper()
+        {
+            Assert.IsTrue(Code.Adapter.Contains("ITransport camelTransport = new CamelToTransportAdapter(camel);"));
+        }
+
+        [TestMethod]
+        public void TestAbstractFactory()
+        {
+            Assert.IsTrue(Code.AbstractFactory.Contains("Hero elf = new Hero(new ElfFactory());"));
+        }
+
+        [TestMethod]
+        public void TestStrategy()
+        {
+            Assert.IsTrue(Code.Strategy.Contains("Car auto = new Car(4, \"Volvo\", new PetrolMove());"));
+        }
+    }
+}
