b86b6b21e36766d1f201edcdc2f4d45bfd8d00d0
Халилов Рустам - LN <konversys@gmail.com>
2019-03-10 01:14:04 +0700

    Добавлены изображения. Исправлены тесты изображений

---
+++ GuessThePattern/Model/Map/Images.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+namespace GuessThePattern.Model.Map
+{
+    public class Images
+    {
+        public static readonly BitmapImage AbstractFactory = new BitmapImage(new Uri(DefaultPath("AbstractFactory"), UriKind.Absolute));
+        public static readonly BitmapImage Adapter = new BitmapImage(new Uri(DefaultPath("Adapter"), UriKind.Absolute));
+        public static readonly BitmapImage Builder = new BitmapImage(new Uri(DefaultPath("Builder"), UriKind.Absolute));
+        public static readonly BitmapImage Command = new BitmapImage(new Uri(DefaultPath("Command"), UriKind.Absolute));
+        public static readonly BitmapImage Decorator = new BitmapImage(new Uri(DefaultPath("Decorator"), UriKind.Absolute));
+        public static readonly BitmapImage FactoryMethod = new BitmapImage(new Uri(DefaultPath("FactoryMethod"), UriKind.Absolute));
+        public static readonly BitmapImage Iterator = new BitmapImage(new Uri(DefaultPath("Iterator"), UriKind.Absolute));
+        public static readonly BitmapImage Prototype = new BitmapImage(new Uri(DefaultPath("Prototype"), UriKind.Absolute));
+        public static readonly BitmapImage Strategy = new BitmapImage(new Uri(DefaultPath("Strategy"), UriKind.Absolute));
+        static string DefaultPath(string name)
+        {
+            return $"C:/Users/Konver/source/repos/GuessThePattern/GuessThePattern/Model/Map/Images/{name}.png";
+        }
+    }
+}
--- Test/ImagesTest.cs
+++ Test/ImagesTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GuessThePattern.Model.Map;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Test
@@ -9,52 +10,52 @@ namespace Test
         [TestMethod]
         public void TestAbstractFactory()
         {
-            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("AbstractFactory"));
+            Assert.AreEqual(DefaultPath("AbstractFactory"), Images.AbstractFactory.UriSource.AbsolutePath);
         }
         [TestMethod]
         public void TestAdapter()
         {
-            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Adapter"));
+            Assert.AreEqual(DefaultPath("Adapter"), Images.Adapter.UriSource.AbsolutePath);
         }
         [TestMethod]
         public void TestBuilder()
         {
-            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Builder"));
+            Assert.AreEqual(DefaultPath("Builder"), Images.Builder.UriSource.AbsolutePath);
         }
         [TestMethod]
         public void TestCommand()
         {
-            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Command"));
+            Assert.AreEqual(DefaultPath("Command"), Images.Command.UriSource.AbsolutePath);
         }
         [TestMethod]
         public void TestDecorator()
         {
-            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Decorator"));
+            Assert.AreEqual(DefaultPath("Decorator"), Images.Decorator.UriSource.AbsolutePath);
         }
         [TestMethod]
         public void TestFactoryMethod()
         {
-            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("FactoryMethod"));
+            Assert.AreEqual(DefaultPath("FactoryMethod"), Images.FactoryMethod.UriSource.AbsolutePath);
         }
         [TestMethod]
         public void TestIterator()
         {
-            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Iterator"));
+            Assert.AreEqual(DefaultPath("Iterator"), Images.Iterator.UriSource.AbsolutePath);
         }
         [TestMethod]
         public void TestPrototype()
         {
-            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Prototype"));
+            Assert.AreEqual(DefaultPath("Prototype"), Images.Prototype.UriSource.AbsolutePath);
         }
         [TestMethod]
         public void TestStrategy()
         {
-            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Strategy"));
+            Assert.AreEqual(DefaultPath("Strategy"), Images.Strategy.UriSource.AbsolutePath);
         }
 
         string DefaultPath(string name)
         {
-            return $"D:/ASTU/8s/ВиТПО/L3/GuessThePattern/GuessThePattern/Model/Map/{name}.png";
+            return $"C:/Users/Konver/source/repos/GuessThePattern/GuessThePattern/Model/Map/Images/{name}.png";
         }
     }
 }
