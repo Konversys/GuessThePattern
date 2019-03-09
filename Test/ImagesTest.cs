779162418658ef5f68fe2f804de2e035c2af6a75
Халилов Рустам - LN <konversys@gmail.com>
2019-03-10 00:31:19 +0700

    Тест картинок

---
+++ Test/ImagesTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class ImagesTest
+    {
+        [TestMethod]
+        public void TestAbstractFactory()
+        {
+            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("AbstractFactory"));
+        }
+        [TestMethod]
+        public void TestAdapter()
+        {
+            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Adapter"));
+        }
+        [TestMethod]
+        public void TestBuilder()
+        {
+            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Builder"));
+        }
+        [TestMethod]
+        public void TestCommand()
+        {
+            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Command"));
+        }
+        [TestMethod]
+        public void TestDecorator()
+        {
+            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Decorator"));
+        }
+        [TestMethod]
+        public void TestFactoryMethod()
+        {
+            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("FactoryMethod"));
+        }
+        [TestMethod]
+        public void TestIterator()
+        {
+            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Iterator"));
+        }
+        [TestMethod]
+        public void TestPrototype()
+        {
+            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Prototype"));
+        }
+        [TestMethod]
+        public void TestStrategy()
+        {
+            Assert.AreEqual(Images.AbstractFactory.BaseUri, DefaultPath("Strategy"));
+        }
+
+        string DefaultPath(string name)
+        {
+            return $"D:/ASTU/8s/ВиТПО/L3/GuessThePattern/GuessThePattern/Model/Map/{name}.png";
+        }
+    }
+}
